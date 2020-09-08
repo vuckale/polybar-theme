f1adb85a47a6ecfc01f5cbb2a2c96a81a6f6be78
vuckale <56733276+vuckale@users.noreply.github.com>
2020-09-08 22:30:04 +0200

    code cleanup and bar spacit + renamed config into config.ini

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ killall -q polybar
 
 # Launch top-left, top and top-right
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
-polybar top-left >>/tmp/polybar1.log 2>&1 & disown
-polybar top >>/tmp/polybar2.log 2>&1 & disown
-polybar top-right >>/tmp/polybar3.log 2>&1 & disown
+polybar --config=~/.config/polybar/config.ini top-left >>/tmp/polybar1.log 2>&1 & disown
+polybar --config=~/.config/polybar/config.ini top >>/tmp/polybar2.log 2>&1 & disown
+polybar --config=~/.config/polybar/config.ini top-right >>/tmp/polybar3.log 2>&1 & disown
 echo "Launched polybar..."
