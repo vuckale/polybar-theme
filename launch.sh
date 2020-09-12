4beac69da41147e10f427848fef24a9facd3f33b
vuckale <56733276+vuckale@users.noreply.github.com>
2020-09-12 18:18:49 +0200

    adding possibility to open calendar and datetime settings from date module

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ killall -q polybar
 HWMON_PATH=$(for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)"; done | grep "coretemp: Core 0" | awk -F' ' '{print $4}')
 export HWMON_PATH
 
-COLOR="default.ini"
+COLOR="autumn_var3.ini"
 
 BACKGROUND=$(grep "background "  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
 export BACKGROUND
@@ -22,8 +22,11 @@ export ALERT
 DATE_MODULE_HELPER="%{F$FOREGROUND_ALT}󰑄%{F-} %H:%M"
 export DATE_MODULE_HELPER
 
-DATE_MODULE_HELPER_ALT="%{F$FOREGROUND_ALT}󰑄%{F-} %H:%M:%S"
+DATE_MODULE_HELPER_ALT="%{F$FOREGROUND_ALT}󰑄 %{A1:gnome-control-center datetime:}󰃰%{A}%{F-} %H:%M:%S"
 export DATE_MODULE_HELPER_ALT
+
+DATE_MODULE_CALENDAR="%a %b %d %Y %{A1:gnome-calendar:}%{F$FOREGROUND_ALT}󰃭%{F-}%{A}"
+export DATE_MODULE_CALENDAR
 
 # Launch top-left, top and top-right
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
