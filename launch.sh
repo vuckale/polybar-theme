927cd868e0e89a246db21e6dcc9f2cdd90a0b522
vuckale <56733276+vuckale@users.noreply.github.com>
2020-09-13 23:57:12 +0200

    added suspend in menu, launch script cleanup and minor norification correction in wifi-switch.sh

--- launch.sh
+++ launch.sh
@@ -2,29 +2,24 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Find path for hwmon (since it may change after reboot)
 HWMON_PATH=$(for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)"; done | grep "coretemp: Core 0" | awk -F' ' '{print $4}')
 export HWMON_PATH
 
+# Exporting colors from $COLOR	as environment variables
 COLOR="autumn_var3.ini"
-
 BACKGROUND=$(grep "background "  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
 export BACKGROUND
-
 FOREGROUND=$(grep "foreground "  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
 export FOREGROUND
-
 FOREGROUND_ALT=$(grep "foreground-alt"  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
 export FOREGROUND_ALT
-
 ALERT=$(grep "alert"  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
 export ALERT
-
 DATE_MODULE_HELPER="%{F$FOREGROUND_ALT}󰑄%{F-} %H:%M"
 export DATE_MODULE_HELPER
-
 DATE_MODULE_HELPER_ALT="%{F$FOREGROUND_ALT}󰑄 %{A1:gnome-control-center datetime:}󰃰%{A}%{F-} %H:%M:%S"
 export DATE_MODULE_HELPER_ALT
-
 DATE_MODULE_CALENDAR="%a %b %d %Y %{A1:gnome-calendar:}%{F$FOREGROUND_ALT}󰃭%{F-}%{A}"
 export DATE_MODULE_CALENDAR
 
--- scripts/wifi-switch.sh
+++ scripts/wifi-switch.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 if [ $(rfkill list wifi | grep "Soft blocked: yes" | wc -l) -gt 0 ] ; then
     rfkill unblock wifi
-    notify-send "Enabled Wireless" "connecting to previously used ssid..."
+    notify-send "wifi-switch.sh" "Enabled Wireless, connecting to previously used ssid..."
 else
     SSID=$(iwgetid -r)
     rfkill block wifi
-    notify-send "Disabled Wireless" "disconnected from ${SSID}"
+    if [ "$SSID" = "" ]; then
+	msg="Disabled Wireless"
+    else
+	msg="Disabled Wireless, disconnected from ${SSID}"
+    fi
+    notify-send "wifi-switch.sh" "$msg"
 fi
