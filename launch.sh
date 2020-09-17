765047d881fdd316508317297022f55d1c7bd366
vuckale <56733276+vuckale@users.noreply.github.com>
2020-09-17 23:02:57 +0200

    launch.sh cleanup and tail = true instead of interval = 1 in bluetooth module

--- launch.sh
+++ launch.sh
@@ -4,24 +4,19 @@ killall -q polybar
 
 # Find path for hwmon (since it may change after reboot)
 HWMON_PATH=$(for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)"; done | grep "coretemp: Core 0" | awk -F' ' '{print $4}')
-export HWMON_PATH
 
-# Exporting colors from $COLOR	as environment variables
+# Parsing colors from $COLOR as environment variables
 COLOR="autumn_var3.ini"
 BACKGROUND=$(grep "background "  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
-export BACKGROUND
 FOREGROUND=$(grep "foreground "  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
-export FOREGROUND
 FOREGROUND_ALT=$(grep "foreground-alt"  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
-export FOREGROUND_ALT
 ALERT=$(grep "alert"  ~/.config/polybar/colors/$COLOR | cut -d'=' -f2 | sed 's/ //g')
-export ALERT
 DATE_MODULE_HELPER="%{F$FOREGROUND_ALT}󰑄%{F-} %H:%M"
-export DATE_MODULE_HELPER
 DATE_MODULE_HELPER_ALT="%{F$FOREGROUND_ALT}󰑄 %{A1:gnome-control-center datetime:}󰃰%{A}%{F-} %H:%M:%S"
-export DATE_MODULE_HELPER_ALT
 DATE_MODULE_CALENDAR="%a %b %d %Y %{A1:gnome-calendar:}%{F$FOREGROUND_ALT}󰃭%{F-}%{A}"
-export DATE_MODULE_CALENDAR
+
+# Exporting variables
+export BACKGROUND FOREGROUND FOREGROUND_ALT ALERT DATE_MODULE_HELPER DATE_MODULE_HELPER_ALT DATE_MODULE_CALENDAR HWMON_PATH
 
 # Launch top-left, top and top-right
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
