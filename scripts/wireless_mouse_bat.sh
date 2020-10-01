18ed3f7a4d89248219d331bd268d51da913a8199
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-02 01:04:02 +0200

    modify mouse_battery module and script wireless_mouse_bat.sh

--- scripts/wireless_mouse_bat.sh
+++ scripts/wireless_mouse_bat.sh
@@ -8,17 +8,27 @@ done
 
 device=$(echo ${sysclass} | sed 's:.*/::')
 device_alt="mouse_$device"
-
 upower="upower -i /org/freedesktop/UPower/devices"
 
+mouse_charging_status=$(cat ${sysclass}/status)
 state=$($upower/$device_alt | grep "percentage" | tr -d -c 0-9)
-	if [ "$state" == 100 ];then
-		echo "󱊣"
-	elif [ "$state" -lt 100 ] && [ "$state" -ge 50 ]; then
-		echo "󱊢"
-	elif [ "$state" -lt 50 ] && [ "$state" -ge 10 ]; then
-		echo "󱊡"
-	elif [ "$state" -lt 10 ]; then
-		echo "󱃍"
+
+if [ "$mouse_charging_status" = "Unknown" ]; then
+	echo "%{F$FOREGROUND_ALT}󰍾%{F-}"
+else
+	if [ "$mouse_charging_status" = "Charging" ]; then
+		prefix=$(echo "%{F$FOREGROUND_ALT}󰦋%{F-}󱐌")
+	else
+		prefix=$(echo "%{F$FOREGROUND_ALT}󰦋%{F-}")
 	fi
+	if [ "$state" == 100 ];then
+		echo "$prefix󱊣"
+	elif [ "$state" -lt 100 ] && [ "$state" -ge 50 ]; then
+		echo "$prefix󱊢"
+	elif [ "$state" -lt 50 ] && [ "$state" -ge 10 ]; then
+		echo "$prefix󱊡"
+	elif [ "$state" -lt 10 ]; then
+		echo "$prefix󱃍"
+	fi
+fi
 
