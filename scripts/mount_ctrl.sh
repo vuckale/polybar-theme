02e1754c1bee9d97bfe04827ade88cf1490522cb
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-07 10:00:00 +0200

    modify mount_ctrl.sh to be able to unmount/power-off drives from polybar

--- scripts/mount_ctrl.sh
+++ scripts/mount_ctrl.sh
@@ -5,8 +5,17 @@ get_mounted_drive_info(){
     while IFS= read -r LINE; do
     DRIVES+=("${LINE}")
     done < <(lsblk -r | grep /media | cut -d' ' -f 7,8 | cut -d'/' -f 4)
+    count=$(echo -e "${#DRIVES[@]}\c")
+    if [ "$count" = "0" ]; then
+	sep=$(echo -e "\c")
+    else
+	sep=$(echo -e " | \c")
+    fi
+    echo -e "%{A1:gnome-disks:}󰋊$sep%{A}\c"
     for drive in ${DRIVES[@]}; do
-        echo -e "%{F$FOREGROUND_ALT}󱊟%{F-} %{T8}%{F$FOREGROUND}$drive%{F-}%{T-} \c"
+        name=$(lsblk -r | grep "$drive" | cut -d' '  -f 1)
+        udiskctl=$(echo "udisksctl unmount -b /dev/$name && notify-send \"mount_ctrl.sh\" \"unmounted $drive\" && sleep 3 && udisksctl power-off -b /dev/$name && notify-send \"mount_ctrl.sh\" \"powered-off $drive\"")
+        echo -e "%{F$FOREGROUND_ALT}󱊟%{F-} %{T8}%{F$FOREGROUND}$drive%{F-}%{T-} %{A1:$udiskctl:}%{F$FOREGROUND_ALT}󰤁%{F-}%{A} | \c"
     done
     printf "\n"
 }
