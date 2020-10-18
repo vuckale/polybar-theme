c2a493da5d22d06591a6df622999eeb1def001a3
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-18 23:59:17 +0200

    scripts formatting

--- scripts/fan_speed.sh
+++ scripts/fan_speed.sh
@@ -2,4 +2,3 @@
 left_fan=$(sensors | grep "Left" | cut -d':' -f2 | cut -d ' ' -f2)
 right_fan=$(sensors | grep "Right" | cut -d':' -f2 | cut -d ' ' -f2)
 echo "%{F$FOREGROUND_ALT}󰈐%{F-} %{F$FOREGROUND_ALT}󰫹%{F-}$left_fan%{F$FOREGROUND_ALT}󰫿%{F-}$right_fan"
-
--- scripts/turbo_boost_and_curr_cpu_freq_status.sh
+++ scripts/turbo_boost_and_curr_cpu_freq_status.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 status=$(cat /sys/devices/system/cpu/intel_pstate/no_turbo)
 freq=$(lscpu | awk -F : '($1=="CPU MHz") {printf "%3.2fGHz\n", $2/1000}')
 if [ $status = 1 ]; then
