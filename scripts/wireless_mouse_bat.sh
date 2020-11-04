4940ec7f7093b2ee57ecf4ad8798f9de25878f30
vuckale <56733276+vuckale@users.noreply.github.com>
2020-11-04 17:39:35 +0100

    if there is no wireless mouse available it prints "X"

--- scripts/wireless_mouse_bat.sh
+++ scripts/wireless_mouse_bat.sh
@@ -6,30 +6,33 @@ for i in /sys/class/power_supply/hidpp_battery_*; do
   sysclass=$(echo "$(<$(dirname $i)): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $i" | grep "hidpp_battery" | awk -F' ' '{print $3}')
 done
 
-device=$(echo ${sysclass} | sed 's:.*/::')
-device_alt="mouse_$device"
-upower="upower -i /org/freedesktop/UPower/devices"
+if [ -d $sysclass ]; then
+	device=$(echo ${sysclass} | sed 's:.*/::')
+	device_alt="mouse_$device"
+	upower="upower -i /org/freedesktop/UPower/devices"
 
-mouse_charging_status=$(cat ${sysclass}/status)
-state=$($upower/$device_alt | grep "percentage" | tr -d -c 0-9)
+	mouse_charging_status=$(cat ${sysclass}/status)
+	state=$($upower/$device_alt | grep "percentage" | tr -d -c 0-9)
 
-if [ "$mouse_charging_status" = "Unknown" ]; then
-	echo "%{F$FOREGROUND_ALT}󰍾%{F-}"
-else
-	prefix=$(echo "%{F$FOREGROUND_ALT}󰦋")
+	if [ "$mouse_charging_status" = "Unknown" ]; then
+		echo "%{F$FOREGROUND_ALT}󰍾%{F-}"
+	else
+		prefix=$(echo "%{F$FOREGROUND_ALT}󰦋")
 
-	if [ "$mouse_charging_status" = "Charging" ]; then
-		echo "$prefix󰂄%{F-}$state%"
-	else 
-		if [ "$state" == 100 ];then
-			echo "$prefix󱊣%{F-}$state%"
-		elif [ "$state" -lt 100 ] && [ "$state" -ge 50 ]; then
-			echo "$prefix󱊢%{F-}$state%"
-		elif [ "$state" -lt 50 ] && [ "$state" -ge 10 ]; then
-			echo "$prefix󱊡%{F-}$state%"
-		elif [ "$state" -lt 10 ]; then
-			echo "$prefix󱃍%{F-}$state%"
+		if [ "$mouse_charging_status" = "Charging" ]; then
+			echo "$prefix󰂄%{F-}$state%"
+		else
+			if [ "$state" == 100 ];then
+				echo "$prefix󱊣%{F-}$state%"
+			elif [ "$state" -lt 100 ] && [ "$state" -ge 50 ]; then
+				echo "$prefix󱊢%{F-}$state%"
+			elif [ "$state" -lt 50 ] && [ "$state" -ge 10 ]; then
+				echo "$prefix󱊡%{F-}$state%"
+			elif [ "$state" -lt 10 ]; then
+				echo "$prefix󱃍%{F-}$state%"
+			fi
 		fi
 	fi
+else
+	echo "X"
 fi
-
